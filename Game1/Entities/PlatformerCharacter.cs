2ca65e5b64903ee31515119396f531e92031bc9f
Jasonjmartel <jasonjmartel@googlemail.com>
2019-04-10 15:57:16 +0100

    Add files via upload

---
+++ Game1/Entities/PlatformerCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1.Entities
+{
+    class PlatformerCharacter : PhysicsEntity, iCollide
+    {
+        //constructor
+        public PlatformerCharacter()
+        {
+            setPosition(mPosition.X, mPosition.Y);
+            mcoll = 36;
+            mSpeed = 0;
+            grav = 0.1F;
+            Fixed = false;
+            mMass = 100;
+            mVelocity.X = -1;
+            damping = 0.1f;
+        }
+
+    }
+}
